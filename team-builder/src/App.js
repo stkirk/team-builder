4aba3d32426aa269070e0b39a981e916e036279f
stkirk <shaanekirk@gmail.com>
2021-07-08 16:19:56 -0400

    imported useState to Form component, initialized formValues and members as state from props

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,11 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 import Form from "./components/Form";
 import "./App.css";
+
+//initial members list that we will set to state
+const membersList = [{ firstname: "Shane", lastname: "Kirk" }];
+
+//initialFormValues will hold initial slice of state for the form, same structure as memberslist
+const initialFormValues = {
+  firstname: "",
+  lastname: "",
+};
 
 function App() {
   return (
     <div className="App">
-      <Form />
+      <Form membersList={membersList} initialFormValues={initialFormValues} />
     </div>
   );
 }
--- team-builder/src/components/Form.js
+++ team-builder/src/components/Form.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Form(props) {
+  //deconstruct intial slices of state passed as props
+  const { initialFormValues, membersList } = props;
+
+  //set our initial values as slices of state
+  const [members, setMembers] = useState(membersList);
+  const [formValues, setFormValues] = useState(initialFormValues);
+
   return (
     <div className="container">
       <h1>Team Builder</h1>
